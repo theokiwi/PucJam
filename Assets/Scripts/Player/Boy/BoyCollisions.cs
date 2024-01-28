6dd6b3a4730a37e9ea31c79dfe8e0a8b3b332dc7
Laprinhas <145940041+Laprinhas@users.noreply.github.com>
2024-01-28 05:51:14 -0300

    4.0
    Mol quase salvou

--- Assets/Scripts/Player/Boy/BoyCollisions.cs
+++ Assets/Scripts/Player/Boy/BoyCollisions.cs
@@ -4,30 +4,41 @@ using UnityEngine;
 
 public class BoyCollisions : ICollisions
 {
-    [SerializeField] 
+    [SerializeField]
     private float forceMagnitude;
     public float maxDistance;
 
     void FixedUpdate()
     {
-        
+
     }
 
     void OnControllerColliderHit(ControllerColliderHit hitCon)
     {
+
+
+        Vector3 direcao = new Vector3();
         Rigidbody rb = hitCon.collider.attachedRigidbody;
 
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward),out hit, maxDistance))
+        RaycastHit[] hit;
+        hit = Physics.RaycastAll(transform.position, transform.TransformDirection(Vector3.forward), 5);
+        if (hit.Length > 0)
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * maxDistance, Color.green);
+            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * maxDistance, Color.green);
             if (hitCon.collider.CompareTag("box"))
             {
+                //direcao = hit.normal * -100;
                 Debug.Log("hit a box");
-                Debug.Log(hit.normal);
-                hit.rigidbody.AddForce(hit.normal * forceMagnitude);
+                Debug.Log(hit.Length);
+                //hit.rigidbody.AddForce(direcao);
+                if (hit.Length == 1)
+                {
+                    hit[0].transform.Translate(hit[0].normal * -2.5f);
+                }
+
             }
         }
-  
+
     }
 }
+
